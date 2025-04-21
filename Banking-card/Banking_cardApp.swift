00399d9ee4ada0482b2ece0d959a580a02953a99
kamran.namazov <kamranamazov@yahoo.com>
2025-04-21 18:12:59 +0400

    Initial Commit

---
+++ Banking-card/Banking_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_cardApp.swift
+//  Banking-card
+//
+//  Created by Kamran on 21.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Banking_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Banking-cardTests/Banking_cardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_cardTests.swift
+//  Banking-cardTests
+//
+//  Created by Kamran on 21.04.25.
+//
+
+import Testing
+@testable import Banking_card
+
+struct Banking_cardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
